e55c8af7d6135690547331ca67aa7a889792896c
Eunjin Jo <euji42@weirdsector.co.kr>
2024-05-16 12:07:28 +0900

    feat: 메인페이지 2번째 영역

--- src/app/(main)/[language]/(home)/page.tsx
+++ src/app/(main)/[language]/(home)/page.tsx
@@ -1,21 +1,34 @@
 'use client'
 
 import { useState, useEffect } from 'react';
-import { useParams, useRouter } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 
 import styles from './page.module.css';
-import { language } from '@Types/type';
+import { language } from '@Types/type'
+import useTranslate from '@Hooks/useTranslate';
+import useResponsive from '@Hooks/useResponsive';
 import burimTypoImage from '@Public/images/main/burim.png';
+import arrowIcon from '@Public/icons/arrow/right-white.svg';
 import ContactUsPage from '@/app/(main)/[language]/contact/page';
 import Bold from '@Components/common/typo/bold/bold';
 import Medium from '@Components/common/typo/medium/medium';
 import Regular from '@Components/common/typo/regular/regular';
+import RoundButton from '@Components/common/buttons/round-button/round-button';
 import LinkCollection from '@Components/main/link-collection/link-collection';
 
-const LanguageHome = () => {
+const LanguageHome = ({ params } : {
+  params: {
+    language: language;
+  }
+}) => {
+  const { t } = useTranslate('home', params.language);
+  const { greaterThanLarge } = useResponsive();
+
   const [mounted, setMounted] = useState(false);
+
+  const summaryObj = JSON.parse(t('body1.summary.description'));
+  const summaryKeys = Object.keys(summaryObj).sort((a: string,b: string) => Number(a.at(-1)) - Number(b.at(-1)));
 
   useEffect(() => {
     setMounted(true);
@@ -85,7 +98,118 @@ const LanguageHome = () => {
       <section
         className={styles.body1}
       >
-        {/* 설명 */}
+        {greaterThanLarge ? (
+          <Image
+            className={styles['destop-map']}
+            src={`/images/main/map-desktop.png`}
+            alt={''}
+            fill
+          />
+        ) : (
+          <Image
+            className={styles['mobile-map']}
+            src={`/images/main/map-mobile.png`}
+            alt={''}
+            fill
+          />
+        )}
+
+        <div
+          className={styles['body1-contents']}
+        >
+          <div
+            className={styles.upward}
+          >
+            <Medium
+              classNames={[
+                'text-[16px] lg:text-[24px]',
+                'text-green-1',
+              ]}
+            >{t('body1.title.sub')}</Medium>
+            <Bold
+              classNames={[
+                'text-[32px] lg:text-[40px]',
+              ]}
+            >{t('body1.title.main')}</Bold>
+            <Regular
+              classNames={[
+                'text-[14px] lg:text-[20px]',
+                styles.description,
+              ]}
+            >{t('body1.description')}</Regular>
+
+            <Link
+              className={styles.link}
+              href={`/${params.language}/company/history`}
+            >
+              <RoundButton
+                bgColor={'green'}
+                px={32}
+                py={16}
+              >
+                <div
+                  className={styles.row}
+                >
+                  <Regular
+                    classNames={[
+                      'text-[14px] lg:text-[20px]',
+                      'text-white',
+                    ]}
+                    mr={20}
+                  >{t('body1.btn')}</Regular>
+                  <Image
+                    src={arrowIcon}
+                    alt={''}
+                    width={16}
+                    height={16}
+                  />
+                </div>
+              </RoundButton>
+            </Link>
+          </div>
+          <div
+            className={styles.downward}
+          >
+            <Bold
+              classNames={[
+                'text-[20px] lg:text-[32px]',
+              ]}
+              mb={8}
+            >{t('body1.summary.title')}</Bold>
+            <ul
+              className={styles['summary-ul']}
+            >
+              {summaryKeys.map((key, idx) => {
+                return (
+                  <li
+                    className={styles['summary-li']}
+                  >
+                    <li
+                      className={styles['summary-first-line']}
+                    >
+                      <Bold
+                        classNames={[
+                          'text-[28px] lg:text-[59px]',
+                        ]}
+                      >{t(`body1.summary.description.${key}.highlight`)}</Bold>
+                      <Bold
+                        classNames={[
+                          'text-[16px] lg:text-[32px]',
+                        ]}
+                      >{t(`body1.summary.description.${key}.highlight-suffix`)}</Bold>
+                    </li>
+                    <Regular
+                      classNames={[
+                        'text-[16px] lg:text-[20px]',
+                      ]}
+                      align={'center'}
+                    >{t(`body1.summary.description.${key}.explan`)}</Regular>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
       </section>
 
       <section
