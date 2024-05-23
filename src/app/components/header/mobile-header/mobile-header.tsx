c79fd44176e6a09860f4f258aec285c93d26561a
Eunjin Jo <euji42@weirdsector.co.kr>
2024-05-23 14:32:02 +0900

    feat: 모바일 햄버거 버튼 열릴 때 언어 변환 버튼 추가

--- src/app/components/header/mobile-header/mobile-header.tsx
+++ src/app/components/header/mobile-header/mobile-header.tsx
@@ -1,14 +1,16 @@
 'use client'
 
-import { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
+import { Fragment, useEffect, useState } from 'react';
+import { useParams, useRouter, usePathname } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
 
 import styles from './mobile-header.module.css';
 import { links } from '@Constants/header-link';
+import { languageList } from '@Constants/header-link';
 import useTranslate from '@Hooks/useTranslate';
 import { language } from '@Types/type';
+import { extractPath } from '@Utils/path';
 import logo from '@Public/logos/logo.png';
 import closeIcon from '@Public/icons/close-white.svg';
 import mobileMenuIcon from '@Public/icons/mobile-menu.svg';
@@ -17,6 +19,8 @@ import Regular from '@Components/common/typo/regular/regular';
 const MobileHeader = () => {
   const { language: languageParam } = useParams<{ language: language }>();
   const { t } = useTranslate('header', languageParam);
+  const router = useRouter();
+  const pathname = usePathname();
 
   const [mounted, setMounted] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
@@ -81,77 +85,121 @@ const MobileHeader = () => {
           className={styles.body}
         >
           <div
-            className={styles['btn-wrapper']}
+            className={styles['body-wrapper']}
           >
-            <button
-              type={'button'}
-              onClick={() => {
-                setIsOpen(false);
-              }}
+            <div
+              className={styles['body-upward']}
             >
-              <Image
-                src={closeIcon}
-                alt={'close menu'}
-                width={24}
-                height={24}
-              />
-            </button>
-          </div>
-          <ul
-            className={styles.menus}
-          >
-            {menus.map((menuKey: string, menuKeyIdx: number) => {
-              return (
-                <li
-                  className={styles.menu}
-                  key={`mobile_header_layer_${menuKey}`}
+              <div
+                className={styles['close-btn-wrapper']}
+              >
+                <button
+                  type={'button'}
                   onClick={() => {
-                    setMenuClickState(() => {
-                      const state = [false, false, false, false];
-                      state[menuKeyIdx] = true;
-
-                      return state; 
-                    })
+                    setIsOpen(false);
                   }}
                 >
-                  <Regular
-                    classNames={[
-                      'text-[20px]',
-                      'text-white',
-                      styles['main-menu'],
-                    ]}
-                  >{menuKey}</Regular>
-                  <ul
-                    className={`${styles['sub-ul']} ${menuClickState[menuKeyIdx] ? styles.show : ''}`}
+                  <Image
+                    src={closeIcon}
+                    alt={'close menu'}
+                    width={24}
+                    height={24}
+                  />
+                </button>
+              </div>
+              <ul
+                className={styles.menus}
+              >
+                {menus.map((menuKey: string, menuKeyIdx: number) => {
+                  return (
+                    <li
+                      className={styles.menu}
+                      key={`mobile_header_layer_${menuKey}`}
+                      onClick={() => {
+                        setMenuClickState(() => {
+                          const state = [false, false, false, false];
+                          state[menuKeyIdx] = true;
+
+                          return state; 
+                        })
+                      }}
+                    >
+                      <Regular
+                        classNames={[
+                          'text-[20px]',
+                          'text-white',
+                          styles['main-menu'],
+                        ]}
+                      >{menuKey}</Regular>
+                      <ul
+                        className={`${styles['sub-ul']} ${menuClickState[menuKeyIdx] ? styles.show : ''}`}
+                      >
+                        {getLinks(menuKey).map((el, idx) => {
+                          return (
+                            <li
+                              className={styles['sub-li']}
+                              key={`mobile_sub_menu_${el}`}
+                            >
+                              <Link
+                                href={`/${languageParam}/${links[menuKey][idx + 1]}`}
+                                onClick={() => {
+                                  setIsOpen(false);
+                                }}
+                                key={`mobile_sub_menu_${el}`}
+                              >
+                                <Regular
+                                  classNames={[
+                                    'text-[16px]',
+                                    'text-white',
+                                  ]}
+                                >{t(`${menuKey}.${idx + 1}`)}</Regular>
+                              </Link>
+                            </li>
+                          );
+                        })}                     
+                      </ul>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+              
+            {/* 언어 번역 버튼 */}
+            <div
+              className={styles['language-btn-container']}
+            >
+              {languageList.map((language, idx) => {
+                return (
+                  <Fragment
+                    key={`mobile_language_${language}`}
                   >
-                    {getLinks(menuKey).map((el, idx) => {
-                      return (
-                        <li
-                          className={styles['sub-li']}
-                          key={`mobile_sub_menu_${el}`}
-                        >
-                          <Link
-                            href={`/${languageParam}/${links[menuKey][idx + 1]}`}
-                            onClick={() => {
-                              setIsOpen(false);
-                            }}
-                            key={`mobile_sub_menu_${el}`}
-                          >
-                            <Regular
-                              classNames={[
-                                'text-[16px]',
-                                'text-white',
-                              ]}
-                            >{t(`${menuKey}.${idx + 1}`)}</Regular>
-                          </Link>
-                        </li>
-                      );
-                    })}                     
-                  </ul>
-                </li>
-              );
-            })}
-          </ul>
+                    <button
+                      className={styles.btn}
+                      
+                      onClick={() => {
+                        router.replace(`/${language.toLocaleLowerCase()}${extractPath(pathname)}`);
+                        setIsOpen(false);
+                      }}
+                    >
+                      <Regular
+                        classNames={[
+                          'text-[16px]',
+                          language.toLocaleLowerCase() === languageParam ? 'text-white' : 'text-grey-300',
+                          language.toLocaleLowerCase() === languageParam ? 'font-semibold' : '',
+                        ]}
+                        align={'center'}
+                      >{language}</Regular>
+                    </button>
+                    {languageList.length - 1 !== idx && (
+                      <span
+                        className={styles.slash}
+                      />
+                    )}
+                  </Fragment>
+                );
+              })}
+            </div>
+          </div>
         </section>
       )}
     </>
