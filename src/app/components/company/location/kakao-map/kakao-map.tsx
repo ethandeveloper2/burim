5095c5d8be940673c6b57cadb0456de0b86143d2
ethan <ethandeveloper2@gmail.com>
2024-05-27 15:44:07 +0900

    Fix: 오시는 길 페이지 > 지도 나타나지 않는 문제 재수정

--- src/app/components/company/location/kakao-map/kakao-map.tsx
+++ src/app/components/company/location/kakao-map/kakao-map.tsx
@@ -17,69 +17,54 @@ const KakaoMap = ({
 }) => {
   const mapEl = useRef<HTMLDivElement>(null);
   const [mounted, setMounted] = useState(false);
+  const [sdkLoaded, setSdkLoaded] = useState(false);
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
+  console.log(sdkLoaded)
+
+
   useEffect(() => {
-    console.log('지도1')
+    // 카카오맵 SDK를 동적으로 로드
+    const script = document.createElement('script');
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_KEY}&autoload=false`;
+    script.onload = () => setSdkLoaded(true); // SDK가 로드되면 상태를 업데이트
+    document.head.appendChild(script);
+  }, []);
+
+  useEffect(() => {
+    console.log('지도1');
+
     if (mounted) {
-      const script = document.createElement('script');
-      script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_KEY}&autoload=false`;
-      script.onload = () => {
-        console.log('지도2')
-        const kakao = window.kakao;
-        if (kakao) {
-          console.log('지도3')
-          kakao.maps.load(() => {
-            // 지도 생성
-            const map = new kakao.maps.Map(mapEl.current, {
-              center: new kakao.maps.LatLng(centerY, centerX),
-              mapTypeId : kakao.maps.MapTypeId.ROADMAP,
-              // level: 6,
-              // angle: 80,
-              // rotation: 1.7
-            }); 
-        
-            // 마커 생성
-            const marker = new kakao.maps.Marker({
-                position: new kakao.maps.LatLng(markerY ? markerY : centerY, markerX ? markerX : centerX),
-                map: map,
-            });
+      console.log('지도2');
+      
+      // 카카오 지도 로드 후 지도 생성
+      const kakao = window.kakao;
+      
+      if (kakao) {
+        console.log('지도3');
+
+        kakao.maps.load(() => {
+          // 지도 생성
+          const map = new kakao.maps.Map(mapEl.current, {
+            center: new kakao.maps.LatLng(centerY, centerX),
+            mapTypeId : kakao.maps.MapTypeId.ROADMAP,
+            // level: 6,
+            // angle: 80,
+            // rotation: 1.7
+          }); 
+      
+          // 마커 생성
+          const marker = new kakao.maps.Marker({
+              position: new kakao.maps.LatLng(markerY ? markerY : centerY, markerX ? markerX : centerX),
+              map: map,
           });
-        }
+        });
       }
     }
-
-    // if (mounted) {
-    //   console.log('지도2')
-      
-    //   // 카카오 지도 로드 후 지도 생성
-    //   const kakao = window.kakao;
-      
-    //   if (kakao) {
-    //     console.log('지도3')
-
-    //     kakao.maps.load(() => {
-    //       // 지도 생성
-    //       const map = new kakao.maps.Map(mapEl.current, {
-    //         center: new kakao.maps.LatLng(centerY, centerX),
-    //         mapTypeId : kakao.maps.MapTypeId.ROADMAP,
-    //         // level: 6,
-    //         // angle: 80,
-    //         // rotation: 1.7
-    //       }); 
-      
-    //       // 마커 생성
-    //       const marker = new kakao.maps.Marker({
-    //           position: new kakao.maps.LatLng(markerY ? markerY : centerY, markerX ? markerX : centerX),
-    //           map: map,
-    //       });
-    //     });
-    //   }
-    // }
-  }, [mounted, centerY, centerX, markerY, markerX]);
+  }, [mounted, centerY, centerX, markerY, markerX, sdkLoaded]);
 
   return (mounted && (
     <div
